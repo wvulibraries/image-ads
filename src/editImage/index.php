5f373a659dc3d9f300f52a083d3a5c915505ea22
David Davis <ddavisgraphics@gmail.com>
2015-05-05 08:30:31 -0400

    Setting Up better paths for edit button

--- src/editImage/index.php
+++ src/editImage/index.php
@@ -1,9 +1,7 @@
 <?php
-    require_once "engineHeader.php";
+    require_once "../engineHeader.php";
     templates::display('header'); 
-    recurseInsert("includes/forms/ImageForm.php","php");    
-
-    // EDIT THE FORM VALUES OF THE FORMBUILDER FORM 
+    recurseInsert("../includes/forms/ImageForm.php","php");    
 ?>
 
 <header> 
@@ -14,11 +12,6 @@
     <div id="editImage">
         {form name="imageAdForm" display="form"} 
     </div>
-</section>
-
-<section> 
-    <a href="addNewImage.php" class="button"> Add New Image </a>
-    <a href="index.php" class="button"> Back to Home </a>
 </section>
 
 <?php
