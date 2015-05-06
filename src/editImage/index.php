1c06d803740324d139fd5d3d4782b22c5b12961c
ddavisgraphics <ddavisgraphics@gmail.com>
2015-05-06 13:44:46 -0400

    Fixed Errror Messages and Updated Weekdays

--- src/editImage/index.php
+++ src/editImage/index.php
@@ -1,21 +1,22 @@
 <?php
     require_once "../engineHeader.php";
-    templates::display('header'); 
-    recurseInsert("../includes/forms/ImageForm.php","php");    
+    templates::display('header');
+    recurseInsert("../includes/forms/ImageForm.php","php");
 ?>
 
-<header> 
+<header>
     <h1> Editing Image and Display Props </h1>
-</header> 
+</header>
 
 <section>
+    {local var="feedbackStatus"}
     <div id="editImage">
-        {form name="imageAdForm" display="form"} 
+        {form name="imageAdForm" display="form"}
     </div>
 </section>
 
 <?php
-    templates::display('footer'); 
+    templates::display('footer');
     recurseInsert("includes/jsIncludes.php","php");
 ?>
 
--- src/includes/currentAdsDisplay.php
+++ src/includes/currentAdsDisplay.php
@@ -1,42 +1,58 @@
 <?php
-    
-    //DB Connection and SQL Statements    
+
+    //DB Connection and SQL Statements
     $localvars = localvars::getInstance();
     $db        = db::get($localvars->get('dbConnectionName')); // TELL WHAT DB TO CONNECT TO
 	$sql       = sprintf("SELECT imageAds.*, displayConditions.dateStart, displayConditions.dateEnd, displayConditions.weekdays, displayConditions.timeStart, displayConditions.timeEnd FROM imageAds LEFT JOIN displayConditions ON displayConditions.imageAdID = imageAds.ID");
 	$sqlResult = $db->query($sql);
-    $data      = NULL;  
+    $data      = NULL;
     $URLpath = "http://$_SERVER[HTTP_HOST]/admin/image_manager";
 
     // Testing the SQL Stuff
 	if ($sqlResult->error()) {
-		print "ERROR GETTING ADS  -- the error -- " . $sqlResult->errorMsg(); 
+		print "ERROR GETTING ADS  -- the error -- " . $sqlResult->errorMsg();
 		return FALSE;
 	}
-    
+
 	if ($sqlResult->rowCount() < 1) {
-		print "NO ADS FOUND"; 
+		print '
+                <div class="getStarted">
+                    <h2> Project Description </h2>
+                    <p>
+                        It looks like you have not added any images yet, so you have come to this page.
+                        The purpose of this application is to manage images for the Library homepage.
+                        There will be many features that you can add and disable as create new images that
+                        will work as controls for how the image will display.
+                    </p>
+                    <h3> Getting Started! </h3>
+                    <p>
+                        For best use, please use images that have been created by a designer to match
+                        size and brand restrictions that may be in place.  Once you have your images,
+                        simply click on <a href="{local var="baseDirectory"}/addNewImage/"> <i class="fa fa-plus-square"></i> Add New Images</a>, located in the left menu.
+                    </p>
+                </div>
+              ';
 		return FALSE;
 	}
 
-    $displayAdRecords = array();  // Create an Array to parse the data in there 
+    $displayAdRecords = array();  // Create an Array to parse the data in there
 
-    // Look at the stuff in the DB and actually fetch it 
+    // Look at the stuff in the DB and actually fetch it
     while($row = $sqlResult->fetch()) {
-        
+
         // A placeholder array that will be used for the basic info from the imageAds Table
         $tempAdArray = array(
             'name'      => htmlSanitize($row['name']),
             'imageAd'   => $row['imageAd'],
-            'ID'        => $row['ID'], 
+            'ID'        => $row['ID'],
             'enabled'   => $row['enabled'],
             'priority'  => $row['priority'],
             'altText'   => htmlSanitize($row['altText']),
             'actionURL' => htmlSanitize($row['actionURL']),
-        ); 
+        );
 
         // The display Options Temp Array
-        // Need to add something to search for if Null or Not Null 
+        // Need to add something to search for if Null or Not Null
 
         $tempDispArray = array(
             'dateStart' => $row['dateStart'],
@@ -44,16 +60,16 @@
             'timeStart' => $row['timeStart'],
             'timeEnd'   => $row['timeEnd'],
             'weekdays'  => $row['weekdays']
-        ); 
-        
-        $displayAdRecords[$row['ID']]["imageInfo"] = $tempAdArray; 
-        $displayAdRecords[$row['ID']]['display'][] = $tempDispArray; 
-    }   
+        );
 
-    
+        $displayAdRecords[$row['ID']]["imageInfo"] = $tempAdArray;
+        $displayAdRecords[$row['ID']]['display'][] = $tempDispArray;
+    }
+
+
     foreach($displayAdRecords as $imageRecords) {
         $imgID; // Create Varible for the ID
-        $imgName; // Creat Variable for the Name 
+        $imgName; // Creat Variable for the Name
 
         // Loop through the first Array which is really a placeholder for all arrays
         print "<ul class='current-images'>";
@@ -61,97 +77,97 @@
         // Loop through interior of arrays to get information such as image properties
         foreach($imageRecords["imageInfo"] as $recordsIndex => $imgProperties) {
             if($recordsIndex == "name") {
-                $imgName = $imgProperties; 
+                $imgName = $imgProperties;
                 print "<h2>" . $imgProperties . "</h2>";
-            } 
-            elseif($recordsIndex == "imageAd"){ 
-                echo '<img src="', $imgProperties ,'"> ';  
+            }
+            elseif($recordsIndex == "imageAd"){
+                echo '<img src="', $imgProperties ,'"> ';
             }
             elseif ($recordsIndex == "ID") {
                 // do nothing so that it doesn't show that to the user
-                $imgID = $imgProperties; 
-            } 
+                $imgID = $imgProperties;
+            }
             else if ($recordsIndex == "enabled"){
-                print "<li>"; 
+                print "<li>";
                 if($imgProperties == 0 ){
-                   print  "<div class='image-status disabled'><span class='display'> <i class='fa fa-eye-slash'></i> Disabled </span></div>";  
-                } 
-                else { 
+                   print  "<div class='image-status disabled'><span class='display'> <i class='fa fa-eye-slash'></i> Disabled </span></div>";
+                }
+                else {
                     print "<div class='image-status enabled'><span class='display'> <i class='fa fa-eye'></i> Enabled </span></div>";
                 }
             }
             else if ($recordsIndex == "priority"){
                 if($imgProperties == 0 ){
-                   print  "<div class='image-priority low'> <span class='display'> <i class='fa fa-circle-thin'></i> Low Priority </span></div>";  
-                } 
-                else { 
+                   print  "<div class='image-priority low'> <span class='display'> <i class='fa fa-circle-thin'></i> Low Priority </span></div>";
+                }
+                else {
                     print "<div class='image-priority high'><span class='display'> <i class='fa fa-exclamation-circle'></i> High Priority </span></div>";
                 }
                 print "</li>";
-            } 
+            }
             else if ($recordsIndex == "altText" ) {
-                print " <h3> Alt Text: </h3> <li>";  
-                print $imgProperties . "</li>"; 
+                print " <h3> Alt Text: </h3> <li>";
+                print $imgProperties . "</li>";
             }
             else if ($recordsIndex == "actionURL" ) {
-                print " <h3> Link: </h3> <li>";  
-                print "<a href='" . $imgProperties . "'>" . $imgProperties . "</a></li>"; 
+                print " <h3> Link: </h3> <li>";
+                print "<a href='" . $imgProperties . "'>" . $imgProperties . "</a></li>";
             }
-            else { 
-                print "<li>" . $imgProperties . "</li>"; 
+            else {
+                print "<li>" . $imgProperties . "</li>";
             }
-        }        
+        }
 
-        print " <h3> Display Dates: </h3> "; 
-        
-        foreach($imageRecords["display"] as $index => $displayRecords) { 
-    
-            foreach($displayRecords as $value => $dispRecord){ 
-                // Print the Values but Only if they aren't Null and have a specific Time 
-                if($value === "dateStart" && !isnull($dispRecord)) { 
-                    print "<li>"; 
+        print " <h3> Display Dates: </h3> ";
+
+        foreach($imageRecords["display"] as $index => $displayRecords) {
+
+            foreach($displayRecords as $value => $dispRecord){
+                // Print the Values but Only if they aren't Null and have a specific Time
+                if($value === "dateStart" && !isnull($dispRecord)) {
+                    print "<li>";
                     print "<span class='start-date-range'>";
-                    print date("m/d/Y", $dispRecord) . " - "; 
+                    print date("m/d/Y", $dispRecord) . " - ";
                     print "</span>";
-                } 
+                }
                 elseif($value === "dateEnd" && !isnull($dispRecord)) {
-                    print "<span class='end-date-range'>"; 
-                    print date("m/d/Y", $dispRecord); 
+                    print "<span class='end-date-range'>";
+                    print date("m/d/Y", $dispRecord);
                     print "</span> </li>";
                 }
             }
         }
 
-        print " <h3> Display Times: </h3> "; 
-        
-        foreach($imageRecords["display"] as $index => $displayRecords) { 
-    
-            foreach($displayRecords as $value => $dispRecord){ 
+        print " <h3> Display Times: </h3> ";
+
+        foreach($imageRecords["display"] as $index => $displayRecords) {
+
+            foreach($displayRecords as $value => $dispRecord){
                if($value === "timeStart" && !isnull($dispRecord)) {
-                    print "<li> <span class='start-time-range'>"; 
-                    print date("h:i a", $dispRecord) .  " - "; 
+                    print "<li> <span class='start-time-range'>";
+                    print date("h:i a", $dispRecord) .  " - ";
                     print "</span>";
                 }
                 elseif($value === "timeEnd" && !isnull($dispRecord)) {
-                    print "<span class='end-time-range'>"; 
-                    print date("h:i a", $dispRecord); 
+                    print "<span class='end-time-range'>";
+                    print date("h:i a", $dispRecord);
                     print "</span> </li>";
-                } 
+                }
             }
         }
 
-        print " <h3> Display Weekdays: </h3> "; 
-        
-        foreach($imageRecords["display"] as $index => $displayRecords) { 
-            foreach($displayRecords as $value => $dispRecord){ 
-               if($value === "weekdays" && !isnull($dispRecord)) { 
-                    print "<li>" . $dispRecord . "</li>"; 
+        print " <h3> Display Weekdays: </h3> ";
+
+        foreach($imageRecords["display"] as $index => $displayRecords) {
+            foreach($displayRecords as $value => $dispRecord){
+               if($value === "weekdays" && !isnull($dispRecord)) {
+                    print "<li><p>" . $dispRecord . "</p></li>";
                 }
             }
         }
 
 
-        $baseDir = $localvars->get('baseDirectory'); 
+        $baseDir = $localvars->get('baseDirectory');
 
         $editDir =  sprintf ("<a href='%s%s' class='%s'> EDIT IMAGE </a>",
                 $baseDir,
@@ -166,10 +182,10 @@
 
         // Setup Buttons to pass the editing of the information into different forms
         print "<li>";
-            print $editDir . " " . $deleteDir; 
+            print $editDir . " " . $deleteDir;
         print "</li>";
 
         print "</ul>";
     }
 
-?> 
+?>
