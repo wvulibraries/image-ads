b73923c710862282151b7c2f53208de7ff5f26d5
Michael Bond <mbond@the-forgotten.org>
2015-05-08 14:23:29 -0400

    Simplified the file a little.

--- src/display.php
+++ src/display.php
@@ -1,32 +1,27 @@
 
 <?php
 	require_once "engineHeader.php";
-    $localvars = localvars::getInstance();
-    $imageID   = $_GET['MYSQL']['imageID'];
 
-    if(validate::getInstance()->integer($imageID)){
-        $sqlID = $imageID;
-    } else {
-        $sqlID = NULL;
-    }
+    $localvars = localvars::getInstance();
+
+    if (!validate::getInstance()->integer($_GET['MYSQL']['imageID'])) {
+        print "";
+        exit;
+    } 
 
     $db        = db::get($localvars->get('dbConnectionName')); // TELL WHAT DB TO CONNECT TO
-    $sql       = sprintf("SELECT * FROM imageAds WHERE imageAds.ID = %s",
-        $sqlID
+    $sql       = sprintf("SELECT * FROM imageAds WHERE imageAds.ID = %s LIMIT 1",
+        $_GET['MYSQL']['imageID']
     );
     $sqlResult = $db->query($sql);
 
-    $imageURI = NULL;
-    while($row = $sqlResult->fetch()) {
-        $imageURI = $row['imageAd'];
-    }
+    $row      = $sqlResult->fetch();
+    $imageURI = $row['imageAd'];
 
-    $imageParts = explode(";", $imageURI); // split on the ; after the mime type
-    $mimeType = substr($imageParts[0], 5); // get the information after the data: text
-    $imageData = substr($imageParts[1],7); // decode the URI
+    $imageParts   = explode(";", $imageURI); // split on the ; after the mime type
+    $mimeType     = substr($imageParts[0], 5); // get the information after the data: text
+    $imageData    = substr($imageParts[1],7); // decode the URI
     $decodedImage = base64_decode($imageData);
-
-
 
     header('Content-Type:' . $mimeType);
     print $imageData;
