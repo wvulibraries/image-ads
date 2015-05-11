a3a9cf2d8720d582a0b905ece9484802b893324b
Michael Bond <mbond@the-forgotten.org>
2015-05-11 13:37:22 -0400

    Don't print anything before the header goes out.

---
+++ src/engineHeaderClean.php
@@ -0,0 +1,15 @@
+<?php
+	require_once '/home/www.libraries.wvu.edu/phpincludes/engine/engineAPI/4.0/engine.php';
+	$engine = EngineAPI::singleton();
+	errorHandle::errorReporting(errorHandle::E_ALL);
+
+	// Set localVars and engineVars variables
+		$localvars  = localvars::getInstance();
+		$enginevars = enginevars::getInstance();
+
+	// These are specific to EngineAPI and pulling the appropriate files
+	// recurseInsert("acl.php","php");
+		recurseInsert("includes/engine/vars.php","php"); // sets vars
+		recurseInsert('includes/engine/engineIncludes.php',"php");
+
+?>
