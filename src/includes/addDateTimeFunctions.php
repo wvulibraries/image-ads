585d46ade75b8926d9365cd95e84ec48d229fe19
ddavisgraphics <ddavisgraphics@gmail.com>
2015-05-28 08:43:53 -0400

    Make time/date endings diff from start

--- src/includes/addDateTimeFunctions.php
+++ src/includes/addDateTimeFunctions.php
@@ -24,9 +24,9 @@ function addDateRanges($options=NULL) {
     $startMonth     = $date->dropdownMonthSelect(1, (isset($options['month']) && checkDateOptionMonth($options['month']))?$options['month']:TRUE, array("id"=>"start_date","name"=>"dateStart[]"));
     $startDay       = $date->dropdownDaySelect((isset($options['day']) && checkDateOptionDay($options['day']))?$options['day']:TRUE, array("id"=>"start_date","name"=>"dateStart[]"));
     $startYear      = $date->dropdownYearSelect(0,5, (isset($options['year']))?$options['year']:TRUE, array("id"=>"start_date","name"=>"dateStart[]"));
-    $endMonth       = $date->dropdownMonthSelect(1, (isset($options['endMonth']) && checkDateOptionMonth($options['endMonth']))?$options['endMonth']:TRUE, array("id"=>"end_date","name"=>"dateEnd[]"));
-    $endDay         = $date->dropdownDaySelect((isset($options['endDay']) && checkDateOptionDay($options['endDay']))?$options['endDay']:TRUE, array("id"=>"end_date","name"=>"dateEnd[]"));
-    $endYear        = $date->dropdownYearSelect(0,5, (isset($options['endYear']))?$options['endYear']:TRUE, array("id"=>"end_date","name"=>"dateEnd[]"));
+    $endMonth       = $date->dropdownMonthSelect(1, (isset($options['endMonth']) && checkDateOptionMonth($options['endMonth']))?$options['endMonth']:(date(n)+1), array("id"=>"end_date","name"=>"dateEnd[]"));
+    $endDay         = $date->dropdownDaySelect((isset($options['endDay']) && checkDateOptionDay($options['endDay']))?$options['endDay']:(date(j)+1), array("id"=>"end_date","name"=>"dateEnd[]"));
+    $endYear        = $date->dropdownYearSelect(0,5, (isset($options['endYear']))?$options['endYear']:(date(Y)+1), array("id"=>"end_date","name"=>"dateEnd[]"));
     $startDateRange = $startMonth . "/" . $startDay . "/" . $startYear;
     $endDateRange   = $endMonth . "/" . $endDay . "/" . $endYear;
 
@@ -38,10 +38,10 @@ function addDateRanges($options=NULL) {
 
 function addTimeRanges($options=NULL) {
     $date      = new date;
-    $startHour = $date->dropdownHourSelect(TRUE, isset($options['startHour'])?$options['startHour']:TRUE, array("name" => "timeStart[]"));
-    $startMin  = $date->dropdownMinuteSelect(15, isset($options['startMin'])?$options['startMin']:TRUE, array("name" => "timeStart[]"));
-    $endHour   = $date->dropdownHourSelect(TRUE, isset($options['endHour'])?$options['endHour']:TRUE, array("name" => "timeEnd[]"));
-    $endMin    = $date->dropdownMinuteSelect(15, isset($options['endMin'])?$options['endMin']:TRUE, array("name" => "timeEnd[]"));
+    $startHour = $date->dropdownHourSelect(TRUE, isset($options['startHour'])?$options['startHour']:TRUE, array("name" => "timeStart[]", "id"=>"time_start" ));
+    $startMin  = $date->dropdownMinuteSelect(15, isset($options['startMin'])?$options['startMin']:TRUE, array("name" => "timeStart[]", "id"=>"time_start"));
+    $endHour   = $date->dropdownHourSelect(TRUE, isset($options['endHour'])?$options['endHour']:(date(G)+1), array("name" => "timeEnd[]", "id"=>"time_end"));
+    $endMin    = $date->dropdownMinuteSelect(15, isset($options['endMin'])?$options['endMin']:TRUE, array("name" => "timeEnd[]", "id"=>"time_end"));
     $startTime = $startHour . " : " . $startMin . "mins";
     $endTime   = $endHour . " : " . $endMin . "mins";
 
