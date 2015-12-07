82d008ad3a61ab8d1b5fb3f7fa84f86b8f8c7b3b
ddavisgraphics <ddavisgraphics@gmail.com>
2015-12-07 14:38:06 -0500

    fix dumb link

--- src/includes/engine/vars.php
+++ src/includes/engine/vars.php
@@ -4,5 +4,5 @@
     $localvars->set('dbConnectionName', 'appDB');
     $localvars->set('pageTitle', 'Image Ad Manager');
     $localvars->set("baseDirectory","/admin/image_manager");
-    $localvars->set("URLpath","https://$_SERVER[HTTP_HOST]/admin/image_manager");
+    $localvars->set("URLpath", "/admin/image_manager");
 ?>
