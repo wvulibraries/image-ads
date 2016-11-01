4d63cc4f54b81c089851b5eec94751be7b640fe9
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2016-11-01 14:31:29 -0400

    start end date updates
    updates to the start end dates. To support adding them in the main ad form.

--- Rotating-Homepage-Ads/app/controllers/start_end_dates_controller.rb
+++ Rotating-Homepage-Ads/app/controllers/start_end_dates_controller.rb
@@ -5,6 +5,13 @@ class StartEndDatesController < ApplicationController
     redirect_to ad_path(@ad)
   end
 
+  def destroy
+    @ad = Ad.find(params[:ad_id])
+    @start_end_date = @ad.start_end_dates.find(params[:id])
+    @start_end_date.destroy
+    redirect_to ad_path(@ad)
+  end
+
   private
   def start_end_date_params
     params.require(:start_end_date).permit(:start_date, :end_date)
--- Rotating-Homepage-Ads/app/models/start_end_date.rb
+++ Rotating-Homepage-Ads/app/models/start_end_date.rb
@@ -1,4 +1,19 @@
 class StartEndDate < ApplicationRecord
   belongs_to :ad
   validates_presence_of :ad
+
+  def hr_start_date
+    return convert_date(self.start_date.to_s)
+  end
+
+  def hr_end_date
+    return convert_date(self.end_date.to_s)
+  end
+
+  private
+  def convert_date(datetime)
+    #time = Time.parse(datetime).in_time_zone('Eastern Time (US & Canada)')
+    date = Time.parse(datetime)
+    return date.strftime("%m-%d-%Y").strip
+  end
 end
