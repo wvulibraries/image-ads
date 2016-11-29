aac59bf1fb709940cae2dfb044636ecb345807ab
ddavisgraphics <dave@ddavisgraphics.com>
2016-11-29 11:14:01 -0500

    documentation and refactoring

--- Rotating-Homepage-Ads/app/models/start_end_date.rb
+++ Rotating-Homepage-Ads/app/models/start_end_date.rb
@@ -2,7 +2,16 @@ class StartEndDate < ApplicationRecord
   belongs_to :ad
   validates_presence_of :ad
   validates :start_date, presence: true
-  validates :end_date, presence: true 
+  validates :end_date, presence: true
+
+  # hr_dates
+  # ==================================================
+  # Name : David Davis
+  # Date : 11/29/2016
+  #
+  # Description:
+  # Both start_date and end date go to conver date, which takes a date/time string and modifies it to a Month - Day - Year sring.
+  # @return (string)
 
   def hr_start_date
     return convert_date(self.start_date.to_s)
@@ -12,9 +21,33 @@ class StartEndDate < ApplicationRecord
     return convert_date(self.end_date.to_s)
   end
 
+  # check date ranges
+  # ==================================================
+  # Name : David Davis
+  # Date : 11/29/2016
+  #
+  # Description:
+  # Looks to see if today is between ranges and returns a boolean
+  # @return (boolean)
+
+  def check_date_ranges
+    today = Date.today
+    return today.between?(self.start_date, self.end_date)
+  end
+
+
+  # convert_date
+  # ==================================================
+  # Name : David Davis
+  # Date : 11/29/2016
+  #
+  # @param datetime (string)
+  # @return Month-Day-Year (string)
+  #
+  # Description:
+  # Returns a string from the Month Day Year as human readable.
   private
   def convert_date(datetime)
-    #time = Time.parse(datetime).in_time_zone('Eastern Time (US & Canada)')
     date = Time.parse(datetime)
     return date.strftime("%m-%d-%Y").strip
   end
--- Rotating-Homepage-Ads/app/models/start_end_time.rb
+++ Rotating-Homepage-Ads/app/models/start_end_time.rb
@@ -2,19 +2,68 @@ class StartEndTime < ApplicationRecord
   belongs_to :ad
   validates_presence_of :ad
   validates :start_time, presence: true
-  validates :end_time, presence: true 
+  validates :end_time, presence: true
 
+  # hr_start_time
+  # ==================================================
+  # Name : David Davis
+  # Date : 11/29/2016
+  #
+  # Description:
+  # Uses convert_time method to return a human readable start time
+  #
+  # Ex:
+  #  Takes a datetime stamp and returns HH:MM AM/PM  [6:00 == 6:00 AM] [14:00 == 2:00 PM]
   def hr_start_time
     return convert_time(self.start_time.to_s)
   end
 
+  # hr_end_time
+  # ==================================================
+  # Name : David Davis
+  # Date : 11/29/2016
+  #
+  # Description:
+  # Uses convert_time method to return a human readable start time
   def hr_end_time
     return convert_time(self.end_time.to_s)
   end
 
+  # check_time_ranges
+  # ==================================================
+  # Name : Tracy McCormick
+  # Date : 11/29/2016
+  #
+  # @return (boolean)
+  #
+  # Description:
+  # looks to see if the start_time and end time is between the current time
+  #
+  # Modified By: David Davis 11/29/16
+  # removed if condition ot return true or false from bewteen method
+  def check_time_ranges
+    now = Time.now.strftime( "%H:%M" )
+    start_time = self.start_time.strftime( "%H:%M" )
+    end_time =  self.end_time.strftime( "%H:%M" )
+    return now.between?(start_time, end_time)
+  end
+
+
   private
+  # convert_time
+  # ==================================================
+  # Name : David Davis
+  # Date : 11/29/2016
+  #
+  # @param datetime (string)
+  # @return (string formatted HH:MM AM/PM)
+  #
+  # Description:
+  # Parses the time string into a time object and then modifies the time string to be formatted HH:MM AM/PM
+  #
+  # Ex:
+  #  Takes a datetime stamp and returns HH:MM AM/PM  [6:00 == 6:00 AM] [14:00 == 2:00 PM]
   def convert_time(datetime)
-    #time = Time.parse(datetime).in_time_zone('Eastern Time (US & Canada)')
     time = Time.parse(datetime)
     return time.strftime("%l:%M %p").strip
   end
