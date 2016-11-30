c3402222c658be21a56e504a10b3686aa3f664c0
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2016-11-30 15:39:43 -0500

    controller testting
    wip : unit testing for the controllers

--- Rotating-Homepage-Ads/test/controllers/ajax_controller_test.rb
+++ Rotating-Homepage-Ads/test/controllers/ajax_controller_test.rb
@@ -1,9 +1,10 @@
-# require 'test_helper'
-#
-# class AjaxControllerTest < ActionDispatch::IntegrationTest
-#   test "should get getads" do
-#     get ajax_getads_url
-#     assert_response :success
-#   end
-#
-# end
+require 'test_helper'
+
+class AjaxControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get getads" do
+    get ajax_getads_url, xhr: true
+    assert_response :success
+  end
+
+end
