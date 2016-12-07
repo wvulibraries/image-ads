521a3661c56ddf212d54c7a68e2f180a71047116
ddavisgraphics <dave@ddavisgraphics.com>
2016-12-07 09:06:41 -0500

    Badge for coverage test

--- Rotating-Homepage-Ads/test/test_helper.rb
+++ Rotating-Homepage-Ads/test/test_helper.rb
@@ -1,9 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
-require 'simplecov'
-SimpleCov.start
-
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require 'simplecov'
+require 'simplecov-shield'
+
+SimpleCov.start
+#SimpleCov.formatter = SimpleCov::Formatter::ShieldFormatter
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
