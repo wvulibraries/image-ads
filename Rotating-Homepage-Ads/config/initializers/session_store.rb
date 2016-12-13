af6d64d5698d3ed7aa4c8382fafa384ae731ada4
ddavisgraphics <dave@ddavisgraphics.com>
2016-12-13 11:14:43 -0500

    Remove Rack Cas Stuff

--- Rotating-Homepage-Ads/config/initializers/session_store.rb
+++ Rotating-Homepage-Ads/config/initializers/session_store.rb
@@ -1,5 +1,2 @@
 # Be sure to restart your server when you modify this file.
-require 'rack-cas/session_store/rails/active_record'
-
 Rails.application.config.session_store :cookie_store, key: '_Rotating-Homepage-Ads_session'
-Rails.application.config.session_store ActionDispatch::Session::RackCasActiveRecordStore
