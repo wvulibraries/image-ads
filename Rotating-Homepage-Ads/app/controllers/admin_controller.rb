2df8bd68927e466614bd58cd65796ca6508b6925
ddavisgraphics <dave@ddavisgraphics.com>
2016-12-29 15:17:01 -0500

    Controller Changes for Checking Permissions, Minor Logic Fixes, Alerts / Feedback Changes.

--- Rotating-Homepage-Ads/app/controllers/admin_controller.rb
+++ Rotating-Homepage-Ads/app/controllers/admin_controller.rb
@@ -4,13 +4,27 @@ class AdminController < ApplicationController
 
   # perform filter before action
   before_filter CASClient::Frameworks::Rails::Filter
+  before_action :check_permissions
 
    def index
      @username = session[:cas_user]
-     @extra_attributes = session[:cas_extra_attributes]
+     @email = session[:cas_extra_attributes][:mail]
+     @last_name = session[:cas_extra_attributes][:sn]
    end
 
    def logout
      CASClient::Frameworks::Rails::Filter.logout(self)
+   end
+
+   private
+   def check_permissions
+    has_permission = User.where(username: session[:cas_user]).exists?
+      if !has_permission
+          redirect_to root_path, error: 'You do not have administrative permissions, please contact the system administrator if you feel that this has been reached in error.'
+      elsif session[:cas_user] == nil && session[:cas_last_valid_ticket]
+        redirect_to root_path, error: 'Something went wrong or a faulty login was detected.'
+      else
+        return true
+      end
    end
 end
--- Rotating-Homepage-Ads/app/controllers/ajax_controller.rb
+++ Rotating-Homepage-Ads/app/controllers/ajax_controller.rb
@@ -1,5 +1,5 @@
 class AjaxController < ApplicationController
   def getads
-    @ads= Ad.ads_shown.sorted_priority
+    @ads = Ad.ads_shown.sorted_priority
   end
 end
