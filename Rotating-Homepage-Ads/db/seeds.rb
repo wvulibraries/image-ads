3eb935ed36ff5d4b43118a4bc03bde7b99ff3ac3
ddavisgraphics <dave@ddavisgraphics.com>
2016-12-29 15:14:01 -0500

    Proper Seed Data

--- Rotating-Homepage-Ads/db/seeds.rb
+++ Rotating-Homepage-Ads/db/seeds.rb
@@ -1,32 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
 def seed_image(file_name, file_type)
-  File.open(File.join(Rails.root, "/app/assets/images/#{file_name}.#{file_type}"))
+  file = File.open(File.join(Rails.root, "/app/assets/images/seed_images/#{file_name}.#{file_type}"), 'rb')
+  return file.read
 end
 
-# filename -text
-# content_type -text
-# file_contents -text
-# image_name - text
-# displayed  boolean
-# priority - int
-# alttext - text
-# link - text
-# selected_days - array ["Days"]
+weekday = Hash.new
+weekday[0] = ["", "Tuesday", "Thursday"];
+weekday[1] = ["", "Monday", "Wednesday", "Friday"];
+weekday[3] = [""];
 
-ads = [
-  [ "hollow.png", "image/png", seed_image('hollow', 'png'), 'Hollow Test Image', 'yes', 10, 'test image of hollow', 'http://www.google.com', ["", "Monday", "Tuesday"] ],
-  [ "test.png", "image/png", seed_image('test', 'png'), 'Test Image', 'yes', 1, 'test image of something else', 'http://www.google.com', [""] ],
-]
 
-ads.each do |filename, mime, file_content, image_name, displayed, priority, alt, link, days|
-  Ad.create(filename:filename, content_type:mime, file_contents:file_content, image_name: image_name, displayed: displayed, priority: priority, alttext: alt, link:link, selected_days: days)
+(1..4).to_a.each do |i|
+  link = 'http://www.google.com';
+  Ad.create(filename:"file_#{i}.png", content_type:"image/png", file_contents:seed_image( i, 'png'), image_name: "Seed Image #{i}", displayed: true, priority: i, alttext: "Sample Alt Text #{i}", link:link, selected_days: weekday[rand(0..2)])
 end
 
 
@@ -60,3 +45,8 @@ times = [
 times.each do | id, st, et|
   StartEndTime.create(start_time: st, end_time: et, ad_id: id)
 end
+
+
+# Create Users
+User.create(username: 'djdavis', firstname: 'David', lastname: 'Davis')
+User.create(username: 'vagrant', firstname: 'Vagrant', lastname: 'Admin')
