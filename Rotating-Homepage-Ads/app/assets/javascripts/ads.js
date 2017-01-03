0024e1d1c5d356db51b0a6185187a316e7e9fd72
ddavisgraphics <dave@ddavisgraphics.com>
2017-01-03 14:15:03 -0500

    Interface Modifications

---
+++ Rotating-Homepage-Ads/app/assets/javascripts/ads.js
@@ -0,0 +1,15 @@
+$(document).on('ready turbolinks:load', function(){
+  if($('.ad-cards').length){
+    $(window).on('load resize', function(){
+      var tallest = 0;
+        $('.ad-cards > li').each(function(){
+            $(this).css('height','auto');
+            if( ($(this).height() + 75) > tallest){
+              tallest = $(this).height() + 75;
+            }
+        });
+
+      $('.ad-cards > li').height(tallest);
+    });
+  }
+});
