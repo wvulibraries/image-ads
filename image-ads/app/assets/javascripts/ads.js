5f3871b1c872b25b4035cbf60a9fea93ef1ca906
Tracy McCormick <tracy.alan.mccormick@gmail.com>
2017-01-24 09:21:18 -0500

    updates to javascript
    updated ads.js to address the buttons and resizing issues.

--- image-ads/app/assets/javascripts/ads.js
+++ image-ads/app/assets/javascripts/ads.js
@@ -2,10 +2,11 @@ $(document).on('ready turbolinks:load', function(){
   if($('.ad-cards').length){
     $(window).on('load resize', function(){
       var tallest = 0;
+      var actionHeight = $('.card-action').height() + 10;
         $('.ad-cards > li').each(function(){
             $(this).css('height','auto');
-            if( ($(this).height() + 75) > tallest){
-              tallest = $(this).height() + 75;
+            if( ($(this).height() + actionHeight) > tallest){
+              tallest = $(this).height() + actionHeight;
             }
         });
 
